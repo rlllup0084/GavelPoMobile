b6e31a34ccf91c4e03dfc7a48b5cc87ba7582f9f
rlllup0084 <llup.renee@gmail.com>
2023-04-14 11:40:02 +0800

    first commit

---
+++ GavelPoMobile.SandBox/Program.cs
@@ -0,0 +1,52 @@
+using GavelPoMobile.SandBox.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GavelPoMobile.SandBox; 
+public class Program {
+    public static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddEndpointsApiExplorer();
+
+        var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<GVLLOCALContext>(o => o.UseSqlServer(connString));
+        builder.Services.AddScoped<GVLLOCALContextProcedures>();
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll",
+                builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+            );
+        });
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment()) {
+            app.UseCors("AllowAll");
+        }
+
+        #region Api codes here...
+
+        // GetOrdersById
+        app.MapGet("api/order/{id}", async ([FromServicesAttribute] GVLLOCALContextProcedures db, int id) =>
+        {
+            var op = new OutputParameter<int>();
+            return await db.GetOrderByIdAsync(id, op);
+        });
+
+        // GetOrderInfinite
+
+        // GetOrdersPagination
+
+        // GetOrders
+
+        // GetPODetailsById
+
+        // GetPOPastApprovals
+
+        #endregion
+
+        app.UseHttpsRedirection();
+        app.Run();
+    }
+}

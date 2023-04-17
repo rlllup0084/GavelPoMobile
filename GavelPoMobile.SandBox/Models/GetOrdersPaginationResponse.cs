b1bc02b3909f525cdf66a24ffce7584933ce3811
rlllup0084 <llup.renee@gmail.com>
2023-04-17 20:26:00 +0800

    Deleted Sanbox Project

--- GavelPoMobile.SandBox/Models/GetOrdersPaginationResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GavelPoMobile.SandBox.Models {
-    public class GetOrdersPaginationResponse {
-        public int Page { get; set; }
-        public int PageSize { get; set; }
-        public int TotalPages { get; set; }
-        public List<GetOrdersPaginationResult>? Results { get; set; }
-    }
-}

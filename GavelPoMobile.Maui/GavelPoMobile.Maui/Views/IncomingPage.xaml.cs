23cc96bee869af7d94acf650b8d5587477527cb1
rlllup0084 <llup.renee@gmail.com>
2023-04-30 12:47:30 +0800

    Updated Incoming DataGrid

--- GavelPoMobile.Maui/GavelPoMobile.Maui/Views/IncomingPage.xaml.cs
+++ GavelPoMobile.Maui/GavelPoMobile.Maui/Views/IncomingPage.xaml.cs
@@ -4,11 +4,22 @@ using GavelPoMobile.Maui.ViewModels;
 namespace GavelPoMobile.Maui.Views;
 
 [XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class IncomingPage : ContentPage
-{
-	public IncomingPage()
-	{
-		InitializeComponent();
+public partial class IncomingPage : ContentPage {
+    public IncomingPage() {
+        InitializeComponent();
         BindingContext = new IncomingViewModel();
+    }
+
+    private void grid_CustomCellAppearance(object sender, CustomCellAppearanceEventArgs e) {
+        e.FontSize = 12;
+        if (e.RowHandle % 2 == 0)
+            e.BackgroundColor = Color.FromArgb("#F7F7F7");
+        if (e.FieldName == "Vendor") {
+            e.FontAttributes = FontAttributes.Bold;
+        }
+        if (e.FieldName == "Total") {
+            e.FontSize = 14;
+            e.FontAttributes = FontAttributes.Bold;
+        }
     }
 }

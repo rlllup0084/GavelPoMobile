df4b12f63da5a55d86169b3da5b3e54709e3a359
rlllup0084 <llup.renee@gmail.com>
2023-05-08 19:28:13 +0800

    Added Status Icon in HistoryPage

---
+++ GavelPoMobile.Maui/GavelPoMobile.Maui/Views/PoStatusImageConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace GavelPoMobile.Maui.Views;
+
+public class PoStatusImageConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+        if (value is int status) {
+            switch (status) {
+                case 0:
+                    return "incoming_1.png";
+                case 1:
+                    return "approve_1.png";
+                case 2:
+                    return "approve_1.png";
+                case 3:
+                    return "approve_1.png";
+                case 4:
+                    return "disapprove_1.png";
+                case 5:
+                    return "pending_1.png";
+            }
+        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+        return null;
+    }
+}
